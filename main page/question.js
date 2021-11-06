b488a3facdd976f2b9647a5d3b90068bf512a41c
YashKaulCS <93838214+YashKaulCS@users.noreply.github.com>
2021-11-06 16:57:25 -0400

    updated question

--- main page/question.js
+++ main page/question.js
@@ -1,8 +1,16 @@
 class Question {
-    construtor(question, answer) {
+    construtor(question, answer, points) {
+        this.points = points;
         this.question = question;
         this.answer = answer;
     }
-
-
+    getQuestion() {
+        return this.question;
+    }
+    getAnswer() {
+        return this.answer;
+    }
+    getPoints() {
+        return this.points;
+    }
 }
