98a838bdec052d8e76579b35a1a350fba5ceecad
EdgValle <vallee2@rpi.edu>
2021-11-06 19:03:49 -0400

    wow a button
    wow a button

--- timer/timer.js
+++ timer/timer.js
@@ -1,15 +1,16 @@
-const startingMinutes = 4;
+const startingMinutes = 1;
 let totalSeconds = startingMinutes * 60;
-
-const timerElem = document.getElementById('timer');
-
+const timerElem = document.getElementById("timer");
+const resultElem = document.getElementById("sellout");
 setInterval(updateTimer, 1000);
-
+function getInput(){
+    resultElem.innerHTML="wow a button";
+}
 function updateTimer(){
     const minutes=Math.floor(totalSeconds/60);
     let seconds=totalSeconds%60;
-    seconds = seconds < 10 ? '0' +seconds : seconds;
-    timerElem.innerHTML = `${minutes}: ${seconds}`;
+    seconds = seconds < 10 ? '0' +seconds: seconds;
+    timerElem.innerHTML = `${minutes}:${seconds}`;
     totalSeconds--;
     totalSeconds=totalSeconds<0?0:totalSeconds;
 }
