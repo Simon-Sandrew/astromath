c8bc612fee6c9676d0da8ada3b6f579d324b1c50
EdgValle <vallee2@rpi.edu>
2021-11-06 17:04:13 -0400

    timer implemented

--- timer/timer.js
+++ timer/timer.js
@@ -1,1 +1,14 @@
-console.log("type here")
+const startingMinutes = 10;
+let totalSeconds = startingMinutes * 60;
+
+const timerElem = document.getElementById('timer');
+
+setInterval(updateTimer, 1000);
+
+function updateTimer(){
+    const minutes=Math.floor(totalSeconds/60);
+    let seconds=totalSeconds%60;
+    seconds = seconds < 10 ? '0' +seconds : seconds;
+    timerElem.innerHTML = `${minutes}: ${seconds}`;
+    totalSeconds--;
+}
