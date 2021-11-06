faa08589c4702aa6c361edd00684efde05dffc78
Simon Sandrew <simon@simonsandrew.com>
2021-11-06 19:02:59 -0400

    more changes

---
+++ index.js
@@ -0,0 +1,3 @@
+particlesJS.load('particle_background', 'styles/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
---
+++ modules.js
@@ -0,0 +1,14 @@
+import { initializeApp } from '@firebase/App';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC1AskYk9ElvioMk-ZZP8uurjzIIGlmZmI",
+    authDomain: "astromath-5a3d6.firebaseapp.com",
+    projectId: "astromath-5a3d6",
+    storageBucket: "astromath-5a3d6.appspot.com",
+    messagingSenderId: "992365971171",
+    appId: "1:992365971171:web:611c9b27a905b311fb27d6",
+    measurementId: "G-QCBREQZ2FE"
+  }
+
+const app = initializeApp(firebaseConfig);
